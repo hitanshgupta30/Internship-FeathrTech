cc8dd585302099d0ab80555453f5ea3208a605d1
Hitansh Gupta <hitanshgupta1930@gmail.com>
2026-07-15 10:46:00 +0530

    Added toggle theme

--- MyPortfolio/script.js
+++ MyPortfolio/script.js
@@ -53,3 +53,16 @@ nodes.forEach(node => {
     });
 });
 
+const themeToggle = document.getElementById('theme-toggle');
+
+themeToggle.addEventListener('click', () => {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    
+    if (currentTheme === 'light') {
+        document.documentElement.removeAttribute('data-theme');
+        themeToggle.textContent = '🌙'; 
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        themeToggle.textContent = '☀️'; 
+    }
+});
